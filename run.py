411fd492d85eae73c86b3f614df4b9cd0f95b0dc
Saif <saifelsayed@gamil.com>
2026-03-31 11:16:46 +0200

    gix run.py for deploy

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import os
-
+from extensions import db
 from app import app
+
+with app.app_context():
+    db.create_all()
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
